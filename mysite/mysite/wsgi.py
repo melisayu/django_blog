e57a5416af651c66d3e342973d70d541083f172a
melisayu <melisayuliakristin@gmail.com>
2020-07-25 01:10:28 +0700

    Fix Heroku1

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,10 +12,13 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/home/path/to/project'
+path = os.path.dirname(os.path.dirname(__file__))
 if path not in sys.path:
     sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
