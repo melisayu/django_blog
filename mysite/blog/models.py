3360af4b53eb1e85facd0a6b6571dd5939b1e6d7
melisayu <melisayuliakristin@gmail.com>
2020-09-06 16:41:37 +0700

    Fix Comment Section Styling

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    main_pic = models.ImageField(upload_to='pictures/', null=True, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
